28c8b7e40c25cca5f570ffa4fcbd31279bea9551
Eselebor Emmanuel <eselebore@gmail.com>
2022-10-18 19:21:47 -0700

    Tasks 2,5, 6-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers.
+ * @numx: The first integer that is to be added.
+ * @numy: The second integer that is to be added.
+ *
+ * Return: The result after the addition.
+ */
+int add(int numx, int numy)
+{
+	return (numx + numy);
+}
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 	{
 		for (la = 'a'; la <= 'z'; la++)
-			_putchar(letter);
+			_putchar(la);
 		_putchar('\n');
 	}
 }
