8f193832d66da1559b2e0de2620418ecb52b54b9
Eselebor Emmanuel <eselebore@gmail.com>
2022-11-15 19:30:55 -0800

    change

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -85,6 +85,7 @@ void util(char **words, char *str)
  * @end: the stopping index of the word
  * @index: the index of the array to insert the word
  */
+
 void create_word(char **words, char *str, int start, int end, int index)
 {
 	int i, j;
